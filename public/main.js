2096fa6808ebb8bb4cc2380b2a19772dd2a41d98
Sattushivaa <satyambhartijnv0@gmail.com>
2025-10-02 17:10:29 +0530

    initialized mongodb setup with lumora

--- public/main.js
+++ public/main.js
@@ -1,2 +1,3 @@
-const server = new WebSocket("wss://lumora-tihk.onrender.com",[]);
+//const server = new WebSocket("wss://lumora-tihk.onrender.com",[]);                    // production
+const server = new WebSocket("ws://localhost:8080",["h"]);                              // development
 server.onmessage=(m)=>console.log(m.data);
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const ws = require("ws").Server;
+const mongoClient = require("mongodb").MongoClient;
+const client = new mongoClient("mongodb+srv://teamlead:Satyam%40123@cluster0.a7kmctr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{});
+async function run(){
+// let x = await client.db("users").collection("creds").insertOne({
+//     username : "sattu",
+//     password : "1234",
+//     role : "user"
+// });
+// console.log(x);
+await client.connect();
 const server = new ws({port:8080});
 
 server.on("connection",(websocket,req)=>{
-    websocket.send("welcome bro!");
+    websocket.on("message",(data)=>{
+        data = JSON.parse("data");
+        switch(data.type){
+            case "login":   manageLogin(data);
+                            break;
+            case "signup":  manageSignup(data);
+                            break;
+            case "rescue_request" :     handleRescueRequest(data);
+                                        break;
+            case "predict_disaster" :   sendDisasterData(data);
+                                        break;
+        }
+    })
 })
+
+}
+run()
